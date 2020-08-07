7dfe8a3b9e53c585307b5398e84b56a2dee369f7
Nathan Flaherty <nateflaherty@gmailcom>
2020-08-07 14:34:34 +0100

    template simply displays posts from database 	modified:   post_list.html 	modified:   ../../../bridging/__pycache__/__init__.cpython-38.pyc 	modified:   ../../../bridging/__pycache__/settings.cpython-38.pyc 	modified:   ../../../bridging/__pycache__/urls.cpython-38.pyc 	modified:   ../../../bridging/__pycache__/wsgi.cpython-38.pyc 	modified:   ../../../myvenv/bin/Activate.ps1 	modified:   ../../../myvenv/bin/activate 	modified:   ../../../myvenv/bin/activate.csh 	modified:   ../../../myvenv/bin/activate.fish 	modified:   ../../../myvenv/pyvenv.cfg

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nathan/Programming/BridgingCoursework/myvenv"
+set -gx VIRTUAL_ENV "/home/nathan/programming/BridgingCoursework/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
